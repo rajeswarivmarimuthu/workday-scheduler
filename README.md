142865d8957a736016ddfb6074f62ad0d9cd36e2
rajeswari marimuthu <rajeswarivmarimuthu@gmail.com>
2022-03-27 16:21:09 -0700

    re-done the work using JQuery, updated the readme.md with revised gif and mobile responsive layout

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Workday Calendar
 
-This is simple calendar application allowing any employee to save events for each hour of the day by modifying starter code. This web applocation runs in the browser and feature dynamically updated HTML and CSS using javascript. 
+This is simple calendar application allowing any employee to save events for each hour of the day by modifying starter code. This web applocation runs in the browser and feature dynamically updated HTML and CSS using JQuery. This application is made mobile responsive using BootStrap grid model.
 
 Used moment.js library to work with time in this web application. Using browser APIs, created and manipulated HTML elements, added event listeners, stored and retrieved data into/from localStorage.
 
@@ -9,6 +9,7 @@ Used moment.js library to work with time in this web application. Using browser 
 - HTML
 - CSS 
 - Javascript 
+- BootStrap
 
 # Application Details
 
@@ -17,9 +18,12 @@ GH Repo @ https://github.com/rajeswarivmarimuthu/workday-scheduler
 Deployed @ https://rajeswarivmarimuthu.github.io/workday-scheduler/
 
 
-# Demo
-
+# Demo 
+## Desktop 
 ![gif](./assets/images/Work-Day-Scheduler.gif)
+
+## Mobile responsive layout
+![image](./assets/images/Mobile-layout.png)
 
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,186 +1,123 @@
 
-// Global Variables declared here!!
-var todayEl = document.getElementById("currentDay")
-var plannerTab = document.getElementById('myTable');
-var today = moment().format('dddd, MMMM Do');
-var currentTimeHH = moment().format('H')
-var currentTime = moment().format('h');
-var startTime = 9;  // must be in 24 hours format!!
-var workingHours = 10; // 
-todayEl.innerText = today;
+$(document).ready(function(){
 
+     $("#currentDay").text(moment().format('dddd, MMMM Do'));
+    var displayHour; 
+    var currentTimeHH = moment().format('H')
+    var startTime = 9;  // must be in 24 hours format!!
+    var workingHours = 10; // 
 
-// Build Time blocks and fetching value from localstorage
-for (i=0;i<workingHours;i++){
-    addTimeBlocks(i, startTime);
-}
-retrieveEventDetails();
+    //Creating Time Blocks!! 
+    for (var i = 0; i<= workingHours; i++) {
 
-// Save event details to localstorage when the values button is clicked
-plannerTab.addEventListener ('click', function(e){
-    e.preventDefault;
-    console.log(e);
-    var arrEvent = []; 
-    let eventDesc;
-    var tdElement;
+        // formatting the time to be displayed in the table for the current day 
+        displayTime = startTime + i; 
+        displayHour = displayHourCalc(displayTime);
 
-    if (e.target.id == 'iconSave') {
-        var iconParent = e.target.parentElement;
-        tdElement = iconParent.parentElement;
-    } else if (e.target.id == 'button') {
-        tdElement = e.target.parentElement;
+        //creating Timeblocks!
+        var rowEl = $('<div>'); 
+        rowEl.addClass ('col-lg-12 col-md-12 col-sm-12 col-12 row');
+
+        //Create hour div + hour para 
+        var hourElDiv = $('<div>');
+        hourElDiv.addClass('col-lg-1 col-md-1 col-sm-1 col-1 p-0');
+        var hourEl = $("<p>"); 
+        hourEl.addClass('hour');
+        hourEl.text (displayHour);
+
+        //Append hour col to row
+        hourElDiv.append(hourEl); 
+        rowEl.append(hourElDiv);
+        
+        //Create event div + event textarea 
+        var eventElDiv = $('<div>');
+        eventElDiv.addClass('col-lg-10 col-sm-10 col-sm-10 col-10 p-0');
+        var eventEl = $("<textarea>");
+        eventEl.addClass('textarea description');
+        eventDescID = "event" + i;
+        eventEl.attr('id', eventDescID);
+        if (currentTimeHH > displayTime) {
+            eventEl.addClass('textarea description past');
+            eventEl.attr('readonly', 'readonly');
+        } else if (currentTimeHH < displayTime) {
+            eventEl.addClass('textarea description future');
+        } else {
+            eventEl.addClass('textarea description present');
+        }
+
+        //Append event to row 
+        eventElDiv.append(eventEl);
+        rowEl.append(eventElDiv);
+
+        //Create event div + event textarea 
+        var buttonElDiv = $('<div>');
+        buttonElDiv.addClass('col-lg-1 col-md-1 col-sm-1 col-1 custom-btnele p-0');
+        var buttonEl = $("<button>");
+        buttonEl.addClass('saveBtn');
+        var btnID = i;
+        buttonEl.attr('id', btnID);
+        
+        // Create icon 
+        var iconEl = $('<i>');
+        iconEl.addClass('fas fa-save');
+        buttonEl.append(iconEl);
+
+        //Append event to row 
+        buttonElDiv.append(buttonEl);
+        rowEl.append(buttonElDiv);
+
+        $(".container").append(rowEl);
+        
+        retrieveEventDetails(i);
+        
     }
 
-    if (tdElement){
-        var trElement = tdElement.parentElement;
-        var rowID = trElement.id;
-        var cellValue = trElement.children[1].innerText;
-        console.log(cellValue);
-            if (cellValue) {
-                var localStorageString = JSON.parse(localStorage.getItem("eventDesc"));
-                if (localStorageString) {
-                    arrEvent = localStorageString;
-                }
-                eventDesc = {
-                    "row" : rowID,
-                    "eventValue" : cellValue
-                }
-                arrEvent.push(eventDesc); 
-                localStorage.setItem("eventDesc", JSON.stringify(arrEvent));
-            }
-    }
-
-    return;
-});
-
-
-///function to add time blocks
-function  addTimeBlocks(rowCnt,startTime) {
-    //define some attributes for table, table row and cells 
-
-    const timeAttributes = {
-        type: 'text',
-        width: '10%',
-        class: 'border-right border-bottom p-1 hour text-right'
-    }
-   
-    const pastDescAtrributes = {
-        width: '85%',
-        class: 'border-right border-bottom p-1 table-secondary description  text-left',
-        id: 'event',
-        contenteditable: 'false'
-    }
-
-    const presentDescAtrributes = {
-        width: '85%',
-        class: 'border-right border-bottom p-1 table-danger description text-left',
-        id: 'event',
-        contenteditable: "true"
-    }
-
-    const futureDescAtrributes = {
-        width: '85%',
-        class: 'border-right border-bottom p-1 table-success description text-left',
-        id: 'event',
-        contenteditable: "true"
-    }
-
-    const actionAttributes = {
-        width: "5%",
-        class: "p-1 border-bottom-0"
-    }     
     
-    const buttonAttributes = {
-        type: 'button',
-        class: 'saveBtn p-1',
-        id: 'button',
+    function displayHourCalc(displayTime) {  
+
+        var displayHour;
+        
+        if (displayTime >= 24) {
+            return;
+         } 
+     
+         if (displayTime > 0 && displayTime < 12) {
+             displayHour = displayTime + " AM";
+         } else if (displayTime < 24 && displayTime > 12) {
+             displayHour = (displayTime - 12) + " PM";
+         } else if (displayTime == 24) {
+             displayHour = (displayTime - 12) + " AM";
+         } else if (displayTime == 12) {
+             displayHour = displayTime + " PM";
+         } else { 
+             console.log ('invalid display time')
+             return;
+         };
+         
+         return displayHour;
     }
 
-    const iconAttributes = {
-        class: 'fas fa-save',
-        id: 'iconSave'
-    }
+    //Storing event description in localStorage unpon button click
+    var saveBtn = $('.saveBtn');
+    saveBtn.on('click',function () {
+         var eventId = $(this).attr('id');
+         var eventDesc = $(this).parent().siblings().children('.description').val();
+         localStorage.setItem(eventId,eventDesc);
 
-    // Inserting rows based on number of working hours
-    var tr = plannerTab.insertRow(rowCnt);  
-    tr = plannerTab.insertRow(rowCnt);
-    trID = 'tr' + rowCnt; 
-    tr.setAttribute("id", trID); 
+    } );
+    
+    //function to retrieve local storage value!!
+    function retrieveEventDetails(storageKey) {
+        var eventDesc = localStorage.getItem(storageKey);
+        console.log(eventDesc);
+        var eventDescID = "event" + storageKey;
+        if (eventDesc) {
+            console.log(eventDescID);
+            $('#'+ eventDescID).text(eventDesc);
+         }
 
-    // formatting the time to be displayed in the table for the current day 
-    displayTime = startTime + rowCnt; 
-    if (displayTime >= 24) {
-       return;
-    } 
-
-    if (displayTime > 0 && displayTime < 12) {
-        displayHour = displayTime + " AM";
-    } else if (displayTime < 24 && displayTime > 12) {
-        displayHour = (displayTime - 12) + " PM";
-    } else if (displayTime == 24) {
-        displayHour = (displayTime - 12) + " AM";
-    } else if (displayTime == 12) {
-        displayHour = displayTime + " PM";
-    } else { 
-        console.log ('invalid display time')
-        return;
     };
-    
-
-    // Inserting the cells to the table and applying the aattributes as mandated
-    for (var c = 0; c < 3; c++) {
-        var td = document.createElement('td');         
-        td = tr.insertCell(c);
-
-        switch(c) {
-            case 0 :    
-                setAttributes(td,timeAttributes)  // Table data
-                td.innerText = displayHour;
-                break;
-            case 1 :
-                if (currentTimeHH > displayTime) {
-                    setAttributes(td, pastDescAtrributes);
-                } else if (currentTimeHH < displayTime) {
-                    setAttributes(td, futureDescAtrributes);
-                } else {
-                    setAttributes(td, presentDescAtrributes);
-                }
-                break;
-            case 2 :
-                setAttributes(td, actionAttributes)
-                var button = document.createElement('button');
-                var buttonIcon = document.createElement("i");
-                setAttributes(buttonIcon,iconAttributes);
-                setAttributes(button,buttonAttributes);
-                td.appendChild(button);
-                button.appendChild(buttonIcon);
-                break;
-        }
-    }
-    return;
-}
+   
+  });
 
 
-//function to apply the styling attributes 
-function setAttributes(element, attributes) {
-    Object.keys(attributes).forEach(attr => {
-      element.setAttribute(attr, attributes[attr]);
-    });
-  }
-
-//Logic to retrieve event details from local storage
-function retrieveEventDetails () {
-    var localStorageString = JSON.parse(localStorage.getItem("eventDesc"));
-    var tableRowEl;
-    var arrEvent = []; 
-    if (localStorageString) {
-        arrEvent = localStorageString;
-        console.log(arrEvent);
-        for (let i =0 ; i <= arrEvent.length - 1; i++ ) {
-            tableRowEl = document.getElementById(arrEvent[i].row);
-            console.log (tableRowEl);
-            tableRowEl.children[1].innerText = arrEvent[i].eventValue;
-        }
-    }
-    
-}
